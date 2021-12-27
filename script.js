8883a4ed9108516577ee75d8fec1286a3565e774
ian_otieno <ian.otieno@student.moringaschol.com>
2021-12-27 10:20:02 +0300

    add google fonts to the header

--- script.js
+++ script.js
@@ -1,96 +1,127 @@
-$(document).ready(function(){
-    $("#designing").click(function(){
+$(document).ready(function () {
+    $("#designing").click(function () {
         $("#design").fadeToggle()
         $("#paragraph").fadeToggle();
     });
-    $("#developing").click(function(){
+    $("#developing").click(function () {
         $("#development").fadeToggle()
         $("#paragraph1").fadeToggle();
     });
-    $("#producing").click(function(){
+    $("#producing").click(function () {
         $("#product").fadeToggle()
         $("#paragraph2").fadeToggle();
     });
-     $("#Images").animate({
-         opacity:1
- 
-        });
-        $("#work1").hover(function() {
-         $(this).stop().animate({opacity:.4},250);
-         $("#carrey").slideToggle();
-     
-     }, function() {
-         $(this).stop().animate({opacity:1},550)
-         $("#carrey").fadeOut();
-     });
-     $("#work2").hover(function() {
-        $(this).stop().animate({opacity:.4},250);
+    $("#Images").animate({
+        opacity: 1
+
+    });
+    $("#work1").hover(function () {
+        $(this).stop().animate({
+            opacity: .4
+        }, 250);
+        $("#carrey").slideToggle();
+
+    }, function () {
+        $(this).stop().animate({
+            opacity: 1
+        }, 550)
+        $("#carrey").fadeOut();
+    });
+    $("#work2").hover(function () {
+        $(this).stop().animate({
+            opacity: .4
+        }, 250);
         $("#pyra").slideToggle();
-    
-    }, function() {
-        $(this).stop().animate({opacity:1},550)
+
+    }, function () {
+        $(this).stop().animate({
+            opacity: 1
+        }, 550)
         $("pyra").fadeOut();
     });
-    $("#work3").hover(function() {
-        $(this).stop().animate({opacity:.4},250);
+    $("#work3").hover(function () {
+        $(this).stop().animate({
+            opacity: .4
+        }, 250);
         $("#mighty").slideToggle();
-    
-    }, function() {
-        $(this).stop().animate({opacity:1},550)
+
+    }, function () {
+        $(this).stop().animate({
+            opacity: 1
+        }, 550)
         $("mighty").fadeOut();
     });
-    $("#work4").hover(function() {
-        $(this).stop().animate({opacity:.4},250);
+    $("#work4").hover(function () {
+        $(this).stop().animate({
+            opacity: .4
+        }, 250);
         $("#studio").slideToggle();
-    
-    }, function() {
-        $(this).stop().animate({opacity:1},550)
+
+    }, function () {
+        $(this).stop().animate({
+            opacity: 1
+        }, 550)
         $("studio").fadeOut();
     });
-    
-    $("#work5").hover(function() {
-        $(this).stop().animate({opacity:.4},250);
+
+    $("#work5").hover(function () {
+        $(this).stop().animate({
+            opacity: .4
+        }, 250);
         $("#have").slideToggle();
-    
-    }, function() {
-        $(this).stop().animate({opacity:1},550)
+
+    }, function () {
+        $(this).stop().animate({
+            opacity: 1
+        }, 550)
         $("#have").fadeOut();
     });
-    $("#work6").hover(function() {
-        $(this).stop().animate({opacity:.4},250);
+    $("#work6").hover(function () {
+        $(this).stop().animate({
+            opacity: .4
+        }, 250);
         $("#word").slideToggle();
-    
-    }, function() {
-        $(this).stop().animate({opacity:1},550)
+
+    }, function () {
+        $(this).stop().animate({
+            opacity: 1
+        }, 550)
         $("#word").fadeOut();
     });
-    $("#work7").hover(function() {
-        $(this).stop().animate({opacity:.4},250);
+    $("#work7").hover(function () {
+        $(this).stop().animate({
+            opacity: .4
+        }, 250);
         $("#mock").slideToggle();
-    
-    }, function() {
-        $(this).stop().animate({opacity:1},550)
+
+    }, function () {
+        $(this).stop().animate({
+            opacity: 1
+        }, 550)
         $("#mock").fadeOut();
     });
-    $("#work8").hover(function() {
-        $(this).stop().animate({opacity:.4},250);
+    $("#work8").hover(function () {
+        $(this).stop().animate({
+            opacity: .4
+        }, 250);
         $("#resta").slideToggle();
-    
-    }, function() {
-        $(this).stop().animate({opacity:1},550)
+
+    }, function () {
+        $(this).stop().animate({
+            opacity: 1
+        }, 550)
         $("#resta").fadeOut();
     });
-    $("form#form").submit(function(event){
+    $("form#form").submit(function (event) {
         // event.preventDefault();
         var name = $("input#fname").val();
         var email = $("input#mail").val();
         var message = $("textarea#message").val();
-        if ($("input#fname").val() && $("input#mail").val()){
-          alert (name + ", we have received your message. Thank you for reaching out to us.");
+        if ($("input#fname").val() && $("input#mail").val()) {
+            alert(name + ", we have received your message. Thank you for reaching out to us.");
+        } else {
+            alert("Please enter your valid name and valid email!");
         }
-        else {
-          alert("Please enter your valid name and valid email!");
-        }
-        
-      });
-});  
+
+    });
+});
