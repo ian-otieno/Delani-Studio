4630f0048077da94f8624232125e95d31a6f21aa
ian_otieno <ian.otieno@student.moringaschol.com>
2021-12-27 09:29:30 +0300

    add $ function for the portfolio section

--- script.js
+++ script.js
@@ -14,5 +14,71 @@ $(document).ready(function(){
      $("#Images").animate({
          opacity:1
  
- 
+        });
+        $("#work1").hover(function() {
+         $(this).stop().animate({opacity:.4},250);
+         $("#carrey").slideToggle();
+     
+     }, function() {
+         $(this).stop().animate({opacity:1},550)
+         $("#carrey").fadeOut();
+     });
+     $("#work2").hover(function() {
+        $(this).stop().animate({opacity:.4},250);
+        $("#pyra").slideToggle();
+    
+    }, function() {
+        $(this).stop().animate({opacity:1},550)
+        $("pyra").fadeOut();
+    });
+    $("#work3").hover(function() {
+        $(this).stop().animate({opacity:.4},250);
+        $("#mighty").slideToggle();
+    
+    }, function() {
+        $(this).stop().animate({opacity:1},550)
+        $("mighty").fadeOut();
+    });
+    $("#work4").hover(function() {
+        $(this).stop().animate({opacity:.4},250);
+        $("#studio").slideToggle();
+    
+    }, function() {
+        $(this).stop().animate({opacity:1},550)
+        $("studio").fadeOut();
+    });
+    
+    $("#work5").hover(function() {
+        $(this).stop().animate({opacity:.4},250);
+        $("#have").slideToggle();
+    
+    }, function() {
+        $(this).stop().animate({opacity:1},550)
+        $("#have").fadeOut();
+    });
+    $("#work6").hover(function() {
+        $(this).stop().animate({opacity:.4},250);
+        $("#word").slideToggle();
+    
+    }, function() {
+        $(this).stop().animate({opacity:1},550)
+        $("#word").fadeOut();
+    });
+    $("#work7").hover(function() {
+        $(this).stop().animate({opacity:.4},250);
+        $("#mock").slideToggle();
+    
+    }, function() {
+        $(this).stop().animate({opacity:1},550)
+        $("#mock").fadeOut();
+    });
+    $("#work8").hover(function() {
+        $(this).stop().animate({opacity:.4},250);
+        $("#resta").slideToggle();
+    
+    }, function() {
+        $(this).stop().animate({opacity:1},550)
+        $("#resta").fadeOut();
+    });
+    
 });  
