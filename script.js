4e83b040587b25f9425073e90134680d324a349f
ian_otieno <ian.otieno@student.moringaschol.com>
2021-12-27 09:26:41 +0300

    add $ fuction for design, development and product section

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("#designing").click(function(){
+        $("#design").fadeToggle()
+        $("#paragraph").fadeToggle();
+    });
+    $("#developing").click(function(){
+        $("#development").fadeToggle()
+        $("#paragraph1").fadeToggle();
+    });
+    $("#producing").click(function(){
+        $("#product").fadeToggle()
+        $("#paragraph2").fadeToggle();
+    });
+     $("#Images").animate({
+         opacity:1
+ 
+ 
+});  
